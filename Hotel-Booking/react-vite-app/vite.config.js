8333f3157e237528f45803b54cba3e0a895f0000
vilhota30 <111867115+vilhota30@users.noreply.github.com>
2024-08-01 17:55:25 +0300

    Видалено субмодуль Hotel-Booking

---
+++ Hotel-Booking/react-vite-app/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default defineConfig({
+  plugins: [react({ include: /\.(js|jsx)$/ })],
+  server: {
+    port: process.env.PORT || 3000,
+    open: true,
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: []
+  },
+  build: {
+    outDir: 'dist'
+  }
+});
